b698777bb711e8df5c90af737f4a7f40b698f79f
Aloey <macintoshvietnam@Aloeey.local>
2022-04-07 11:23:52 +0700

    Second commit problem solved

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import BlankLayout from "../layouts/BlankLayout";
 import MainLayout from "../layouts/MainLayout";
 import DetailPage from "../pages/DetailPage";
